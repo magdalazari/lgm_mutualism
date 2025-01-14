cfe9d0203f8c54d88ecba22b4a230c771051b0e5
Nicholas Judd <nickkjudd@gmail.com>
2025-01-14 18:20:14 +0100

    added cfa & looking at other stuff

--- Troubleshooting.R
+++ Troubleshooting.R
@@ -1,13 +1,9 @@
 #loading packages
-library(lavaan)
-library(ggplot2)
-library(ggcorrplot)
-library(tidyverse)
 
+pacman::p_load(lavaan, ggplot2, ggcorrplot, tidyverse)
 rm(list=ls())
 
-uncorrected_wm_visit <- readRDS("uncorrected_wm_visit.rds")
-
+uncorrected_wm_visit <- readRDS("~/surfdrive/Shared/uncorrected_wm_visit.rds")
 
 picvocab_uncorrected <-data.frame(uncorrected_wm_visit$ID, uncorrected_wm_visit$eventname, uncorrected_wm_visit$pic_vocab)
 
@@ -90,6 +86,24 @@ picvocab_T3~~a*picvocab_T3
 fit_picvocab_basis_complete<- growth(basis_picvocab, data=picvocab_complete)
 summary(fit_picvocab_basis_complete, fit.measures = TRUE, rsquare = TRUE, standardized = TRUE)
 
+# nkj seeing the rand slope differences
+
+head(predict(fit_picvocab_basis_complete)); head(predict(fit_picvocab_linear_complete))
+
+# intercepts are almost perfectly correlated
+cor(predict(fit_picvocab_basis_complete)[,1], predict(fit_picvocab_linear_complete)[,1]) # 0.9999941
+# slopes are almost perfectly correlated
+cor(predict(fit_picvocab_basis_complete)[,2], predict(fit_picvocab_linear_complete)[,2]) # 0.9995805
+
+
+# getting the individual dievations; by subtracting the intercept from the slope
+# in the linear model 4.447 is the change in 1 unit
+# in the basis model 8.895 is the change until 1; 
+# since we have 2 time points (after the intercept) we can divide by 2* (*i think) seems to make sense
+
+head((predict(fit_picvocab_basis_complete)[,2]- 8.895)/2)
+head(predict(fit_picvocab_linear_complete)[,2]- 4.447)
+
 #####Reading 
 
 #Linear 
@@ -118,6 +132,32 @@ reading_T3~~a*reading_T3
 '
 fit_reading_basis_complete<- growth(basis_reading, data=reading_complete)
 summary(fit_reading_basis_complete, fit.measures = TRUE, rsquare = TRUE, standardized = TRUE)
+
+# doing a bivariate-CFA to see if the prediction are equal to the std cov
+
+biCFA <- '
+reading =~ reading_T1 + reading_T2 + reading_T3
+reading ~~ 1*reading
+reading_T1 ~~ reading_T1
+reading_T2 ~~ reading_T2
+reading_T3 ~~ reading_T3
+
+
+picvocab =~ picvocab_T1 + picvocab_T2 + picvocab_T3
+picvocab ~~ 1*picvocab
+picvocab_T1 ~~ picvocab_T1
+picvocab_T2 ~~ picvocab_T2
+picvocab_T3 ~~ picvocab_T3
+
+reading ~~ picvocab
+'
+
+biCFA_mod <- lavaan(biCFA, data = picvocab_reading_complete)
+summary(biCFA_mod, fit.measures = TRUE, rsquare = TRUE, standardized = TRUE)
+
+head(predict(biCFA_mod))
+
+cor(predict(biCFA_mod)[,1], predict(biCFA_mod)[,2])
 
 
 #Combined picvocab and reading linear 
